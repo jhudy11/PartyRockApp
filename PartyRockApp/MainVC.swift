6b8f07c7cefb23807b7b22e05d452c398e5f4936
Joshua Hudson <jhudy11@hotmail.com>
2017-04-16 11:55:32 -0700

    UI Setup Complete

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,22 @@
+//
+//  MainVC.swift
+//  PartyRockApp
+//
+//  Created by Joshua Hudson on 4/16/17.
+//  Copyright © 2017 ParanoidPenguinProductions. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
